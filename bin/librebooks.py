ece84daf4881169b6d6d229347acbd76ba6f0fa7
hughman98 <hughwright98@gmail.com>
2019-12-06 18:05:46 -0600

    Bug Fixes

--- bin/librebooks.py
+++ bin/librebooks.py
@@ -132,12 +132,11 @@ def select_acct():
         cursor = db.cursor()
         index = 0
         if "cred-submit" in request.form:
-            cursor.execute("SELECT name, type, acc_id FROM account join (owns join can_access on owns.id=comp_id) on acc_id=account.id join "
-                           "WHERE type=%s && user = %s", [credTyp, session['user'][0]])
+            cursor.execute("SELECT name, type, acc_id FROM account join (owns natural join can_access) on acc_id=account.id "
+                           "WHERE type=%s AND user_id=%s", [credTyp, session['user'][0]])
         else:
-            cursor.execute(
-                "SELECT name, type, acc_id FROM account join (owns join can_access on owns.id=comp_id) on acc_id=account.id join "
-                "WHERE type=%s && user = %s", [debTyp, session['user'][0]])
+            cursor.execute("SELECT name, type, acc_id FROM account join (owns natural join can_access) on acc_id=account.id "
+                           "WHERE type=%s AND  user_id=%s", [debTyp, session['user'][0]])
         for acct in cursor:
             if index > 5:
                 break
