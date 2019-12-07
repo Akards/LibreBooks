42798cb657279ab9cf1d5e29b1b21a81f488aa81
hughman98 <30803969+hughman98@users.noreply.github.com>
2019-12-06 18:38:58 -0600

    Stopped redifining tran_accts whenever the page loaded

--- bin/librebooks.py
+++ bin/librebooks.py
@@ -71,7 +71,10 @@ def create_tran():
     global debTyp
     global credTyp
     global typ
-    tran_accts = []
+    try:
+        tran_accts
+    except NameError: #If the variable doesn't already exist, define it
+        tran_accts = []
     if request.method == "POST":
         db = get_db()
         cursor = db.cursor()
